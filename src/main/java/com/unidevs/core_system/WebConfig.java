51b5d333e763869a44a0f12738ff722aa2d00244
gasbriel07sm <gasmiranda07@gmail.com>
2025-10-19 20:33:12 -0300

    Documentação GlobalExceptionHandler e WebConfig

--- src/main/java/com/unidevs/core_system/WebConfig.java
+++ src/main/java/com/unidevs/core_system/WebConfig.java
@@ -4,31 +4,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/* @Configuration: é uma fonte de configurações para a aplicação
- * Executa os métodos para customizar o comportamento padrão
- * Detecta esta classe durante a inicialização e aplicar as configurações definidas
+/**
+ * Classe de configuração Web da aplicação.
+ * Implementa a interface {@link WebMvcConfigurer} para customizar o comportamento do Spring MVC em relação
+ * ao tratamento de recursos estáticos.
+ *
+ * Permite o acesso a arquivos armazenados localmente (como imagens enviadas pelo usuário) através de URLs públicas.
+ *
+ * @WebConfig
+ * Configura o mapeamento de recursos estáticos, permitindo o acesso via URL os arquivos salvos no diretório "uploads"
+ * Assim, um arquivo armazenado como uploads/capa.jpg poderá ser acessado publicamente via:
+ * http://localhost:8080/uploads/capa.jpg
+ *
+ * Parâmetros:
+ * @param registry Registro de manipuladores de recursos, usado para adicionar novas localizações estáticas.
  */
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    /* 'implements WebMvcConfigurer': oferece um conjunto de métodos "vazios" que podemos sobrescrever (@Override) para customizar diferentes partes do framework Spring Web MVC, como formatadores, interceptadores, e, neste caso, manipuladores de recursos estáticos.
-     */
-
-    /* @Override: indica que estamos sobrescrevendo um metodo da interface WebMvcConfigurer.
-       public void addResourceHandlers(ResourceHandlerRegistry registry): nos dá um "registro" onde podemos adicionar nossos próprios manipuladores de recursos (resource handlers).
-     */
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // 1. registry.addResourceHandler("/uploads/**"):
-        //    Define o padrão da URL que será interceptado por esta regra.
-        //    "/uploads/**" significa "qualquer requisição que comece com /uploads/".
-        //    O duplo asterisco (**) é um curinga que corresponde a qualquer caminho recursivamente.
         registry.addResourceHandler("/uploads/**")
-                // 2. .addResourceLocations("file:uploads/"):
-                //    Define o local FÍSICO no sistema de arquivos onde o Spring deve procurar os arquivos correspondentes ao padrão da URL.
-                //    "file:": procurar em um caminho  no sistema de arquivos do servidor, em vez de procurar dentro do classpath do projeto (dentro do arquivo .jar).
-                //    "uploads/": procurar por uma pasta chamada 'uploads' no mesmo diretório onde a aplicação foi iniciada.
                 .addResourceLocations("file:uploads/");
     }
 }
